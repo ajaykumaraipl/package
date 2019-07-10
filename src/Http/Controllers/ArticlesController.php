bc71c2bee350b8759a3b1c4805d9f3d5b6610e34
ajaykum.aipl <ajaykum.aipl@gmail.com>
2019-07-10 18:49:27 +0530

    delete multiplea cropped images

--- src/Http/Controllers/ArticlesController.php
+++ src/Http/Controllers/ArticlesController.php
@@ -58,29 +58,29 @@ class ArticlesController extends BaseController
             File::makeDirectory($thumbnail, $mode = 0777, true, true);
             File::makeDirectory($cover, $mode = 0777, true, true);
         }
-
+        
         $image->move($basePath, $newName);
-
+        
         Image::make($basePath."/".$newName)->save($basePath."/".$newName);
         Image::make($basePath."/".$newName)->resize(320, 240)->save($thumbnail."/".$newName);
         Image::make($basePath."/".$newName)->resize(820, 312)->save($cover."/".$newName);
-
+        
         $submittedData = array(
             'category_id' => $request['category_id'],
             'title' => $request['title'],
             'slug' => $request['slug'],
             'content' => $request['content'],
-            'image' => 'uploads/package/img/'.$newName,
+            'image' => $newName,
             'status' => $request['status'],
             'date' => $request['date']
         );
         $lastInsertedId = Articles::storArticle($submittedData);
-
+        
         $tags = new ArticlesTag(array(
             'article_id' => $lastInsertedId,
             'tag_id' => $request['tags']
         ));
-
+        
         $result = ArticlesTag::articlesTag($tags);
         
         if ($result) {
@@ -97,11 +97,11 @@ class ArticlesController extends BaseController
         $tags = Tags::getAllTags();
         return view('view::articles.edit', ['singleNews' => $singleArticles, "categories" => $categories, "tags" => $tags]);
     }
-
+    
     public function update(Request $request, $id)
     {
         $request = Request::all();
-
+        
         // validate the data
         $validator = Validator::make($request, [
             "category_id" => "required",
@@ -112,40 +112,40 @@ class ArticlesController extends BaseController
             "image" => "required",
             "date" => "required",
             "tags" => "required"
-        ]);
-        
-        if ($validator->fails()) {
-            return redirect()->back()->withInput()->withErrors($validator);
-        }
-
-        $image = Request::file('image');
-        $newName = rand().'_'.$image->getClientOriginalName();
-        $basePath = public_path('uploads/package/img');
-        if (!File::exists($basePath)) {
-            File::makeDirectory($basePath, $mode = 0777, true, true);
-        }
-
-        $image->move($basePath, $newName);
-        
-        $singleArticles = Articles::getSingleArticles($id);
-        $singleArticles = array(
-            'category_id' => $request['category_id'] ?: $singleArticles['category_id'],
+            ]);
+            
+            if ($validator->fails()) {
+                return redirect()->back()->withInput()->withErrors($validator);
+            }
+            
+            $image = Request::file('image');
+            $newName = rand().'_'.$image->getClientOriginalName();
+            $basePath = public_path('uploads/package/img');
+            if (!File::exists($basePath)) {
+                File::makeDirectory($basePath, $mode = 0777, true, true);
+            }
+            
+            $image->move($basePath, $newName);
+            
+            $singleArticles = Articles::getSingleArticles($id);
+            $singleArticles = array(
+                'category_id' => $request['category_id'] ?: $singleArticles['category_id'],
             'title' => $request['title'] ?: $singleArticles['title'],
             'slug' => $request['slug'] ?: $singleArticles['slug'],
             'content' => $request['content'] ?: $singleArticles['content'],
-            'image' => 'uploads/package/img/'.$newName ?: $singleArticles['image'],
+            'image' => $newName ?: $singleArticles['image'],
             'status' => $request['status'] ?: $singleArticles['status'],
             'date' => $request['date'] ?: $singleArticles['date']
         );
-
+        
         $result = Articles::updateArticle($singleArticles, $id);
-
+        
         if ($result) {
             $tags = new ArticlesTag(array(
                 'article_id' => $id,
                 'tag_id' => $request['tags']
             ));
-    
+            
             $result = ArticlesTag::articlesTag($tags);
             if ($result) {
                 return redirect('/news');
@@ -154,11 +154,13 @@ class ArticlesController extends BaseController
         
         return view('view::articles.404');
     }
-
+    
     public function delete($id)
     {
         $singleArticles = Articles::getSingleArticles($id);
-        File::delete($singleArticles->image);
+        File::delete('uploads/package/img/'.$singleArticles->image);
+        File::delete('uploads/package/img/thumbnail/'.$singleArticles->image);
+        File::delete('uploads/package/img/cover/'.$singleArticles->image);
         $articles = Articles::deleteArticles($id);
         $articles = ArticlesTag::deleteArticlesTag($id);
         if ($articles) {
