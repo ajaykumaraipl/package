4d51d756cb74746edc985f889bc115386d091a15
ajaykum.aipl <ajaykum.aipl@gmail.com>
2019-07-18 10:50:56 +0530

    change migrations name : there were duplicate name of migratios in the package

--- src/database/migrations/2015_08_04_130507_create_articles_categories_table.php
+++ src/database/migrations/2015_08_04_130507_create_articles_categories_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateArticleTagTable extends Migration
+class CreateArticleCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
--- src/database/migrations/2015_08_04_130507_create_articles_tags_table.php
+++ src/database/migrations/2015_08_04_130507_create_articles_tags_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateArticleTagTable extends Migration
+class CreateArticlesTagsTable extends Migration
 {
     /**
      * Run the migrations.
